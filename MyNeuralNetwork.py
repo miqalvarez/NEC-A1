8965c738e5dafb6ab01b42e2a71a47f1efd79619
Miquel Álvarez <miquel.alvarez@urv.cat>
2023-11-18 18:47:30 +0100

    Add read and split

--- MyNeuralNetwork.py
+++ MyNeuralNetwork.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 
 # Neural Network class
@@ -6,12 +7,13 @@ class MyNeuralNetwork:
   def __init__(self, layers, activation_function, derivative_function, perc, epochs, learning_rate, momentum):
     self.L = len(layers)                  # number of layers
     self.n = layers.copy()                # number of neurons in each layer
+    
     self.h = []                           # field values.
     for lay in range(self.L):
       self.h.append(np.zeros(layers[lay]))
 
     self.theta = []                       # threshold values
-    for lay in range(1, self.L):
+    for lay in range(self.L):
       self.theta.append(np.zeros(layers[lay]))
 
     self.delta = []                       # delta values
@@ -20,30 +22,26 @@ class MyNeuralNetwork:
     
     self.d_w = []                         # delta weights
     self.d_w.append(np.zeros((1, 1)))
-    for lay in range(1, self.L):
+    for lay in range(self.L):
       self.d_w.append(np.zeros((layers[lay], layers[lay - 1])))
 
     self.d_theta = []                     # delta thresholds
-    for lay in range(1, self.L):
+    for lay in range(self.L):
       self.d_theta.append(np.zeros(layers[lay]))
 
-    self.d_w_prev = []                    # previous delta weights
-    self.d_w_prev.append(np.zeros((1, 1)))
-    for lay in range(1, self.L):
-      self.d_w_prev.append(np.zeros((layers[lay], layers[lay - 1])))
+    self.d_w_prev = [np.zeros((1, 1))] + [np.zeros((layers[lay], layers[lay - 1])) for lay in range(1, self.L)]
+
 
     self.d_theta_prev = []                # previous delta thresholds
-    for lay in range(1, self.L):
+    for lay in range(self.L):
       self.d_theta_prev.append(np.zeros(layers[lay]))
     
     self.xi = []                          # node values
     for lay in range(self.L):
       self.xi.append(np.zeros(layers[lay]))
 
-    self.w = []                           # edge weights
-    self.w.append(np.zeros((1, 1)))
-    for lay in range(1, self.L):
-      self.w.append(np.zeros((layers[lay], layers[lay - 1])))
+    self.w = [np.zeros((1, 1))] + [np.zeros((layers[lay], layers[lay - 1])) for lay in range(1, self.L)]
+
 
     self.perc = perc                      # percentage of training data
     self.n_epochs = epochs                # number of epochs
@@ -51,136 +49,151 @@ class MyNeuralNetwork:
     self.momentum = momentum              # momentum
     self.fact = activation_function       # activation function
     self.derivative = derivative_function # derivative of activation function
+    self.errors_train = []
+    self.errors_valid = []
     
-    # Initialize all weights and thresholds randomly
-    for lay in range(1, self.L):
-      self.w[lay] = np.random.rand(self.n[lay], self.n[lay - 1]) - 0.5
-      self.theta[lay] = np.random.rand(self.n[lay]) - 0.5  
+    
+    # Initialize weights and thresholds
+    for lay in range(self.L):
+        for neuron in range(self.n[lay]):
+            # Si es la capa de entrada, no hay pesos para inicializar
+            if lay > 0:
+                for j in range(self.n[lay - 1]):
+                    # Asegúrate de que las dimensiones de self.w sean correctas
+                    self.w[lay][neuron][j] = np.random.uniform(-1, 1)
+                    self.d_w_prev[lay][neuron][j] = 0
 
-    ### FUNCTIONS ###
-    # Train the neural network
-    # X: training samples
-    # y: target values
-    def fit(self, X, y):
-      errors_train = []
-      errors_valid = []
+            # Solo inicializa los umbrales para las capas ocultas y de salida
+            if lay > 0 and lay < self.L - 1:
+                self.theta[lay][neuron] = np.random.uniform(-1, 1)
+                self.d_theta_prev[lay][neuron] = 0
 
-      if self.perc != 0:
-          n_train = int(self.perc * len(X))
-          training_set = X[:n_train]
-          y_train = y[:n_train]
-          validation_set = X[n_train:]
-          y_valid = y[n_train:]
-      else:
-          training_set = X
-          y_train = y
-          validation_set = None
-          y_valid = None
+    
+  ### FUNCTIONS ###
+  # Train the neural network
+  # X: training samples
+  # y: target values
+  def fit(self, X, y):
+    self.errors_train = []
+    self.errors_valid = []
 
-      for epoch in range(1, self.n_epochs):
-        used = []
-        for pat in range(n_train):
-          # Choose a random pattern xµ from the training set
+    if self.perc != 0:
+        n_train = int(self.perc * len(X))
+        training_set = X[:n_train]
+        y_train = y[:n_train]
+        validation_set = X[n_train:]
+        y_valid = y[n_train:]
+    else:
+        training_set = X
+        y_train = y
+        validation_set = None
+        y_valid = None
+
+    for epoch in range(1, self.n_epochs):
+      print("Epoch: ", epoch)
+      used = []
+      for pat in range(n_train):
+        # Choose a random pattern xµ from the training set
+        i = np.random.randint(0, n_train)
+        while i in used:
           i = np.random.randint(0, n_train)
-          while i in used:
-            i = np.random.randint(0, n_train)
 
-          used.append(i)
+        used.append(i)
 
-          xu = training_set[i]
-          yu = y_train[i]
+        xu = training_set[i]
+        yu = y_train[i]
 
-          __feed_forward(xu)
-          __back_propagation(yu)
-          __update_weights()
+        self.__feed_forward(xu)
+        self.__back_propagation(yu)
+        self.__update_weights()
 
-        # Feed−forward all training patterns and calculate their prediction quadratic error
-        for pat in training_set:
-          __feed_forward(pat)
+      # Feed−forward all training patterns and calculate their prediction quadratic error
+      for pat in training_set:
+        self.__feed_forward(pat)
 
-          # Calculate error_train
-          errors_train[epoch][0] = epoch
-          errors_train[epoch][1] = 0
+        # Calculate error_train
+        self.errors_train[epoch][0] = epoch
+        self.errors_train[epoch][1] = 0
+        
+        for neuron in range(self.n[self.L - 1]):
+          self.errors_train[epoch][1] += pow(self.xi[self.L - 1][neuron] - y_train[pat], 2)
+      
+      self.errors_train[epoch][1] /= 2
+        
+      # Feed−forward all validation patterns and calculate their prediction quadratic error
+      if self.validation_set != None:
+
+        for pat in validation_set:
+          self.__feed_forward(pat)
+          
+          # Calculate error_valid
+          self.errors_valid[epoch][0] = epoch
+          self.errors_valid[epoch][1] = 0
           
           for neuron in range(self.n[self.L - 1]):
-            errors_train[epoch][1] += pow(self.xi[self.L - 1][neuron] - y_train[pat], 2)
+            self.errors_valid[epoch][1] += pow(self.xi[self.L - 1][neuron] - y_valid[pat], 2)
+          
+        self.errors_valid[epoch][1] /= 2
+
+      else:
+        self.errors_valid[epoch] = None
+
+  # Feed−forward propagation of pattern xµ to obtain the output o(xµ)
+  def __feed_forward(self, xu):
+    for neuron in range(self.n[0]):
+      self.xi[0][neuron] = xu[neuron]
+
+    for lay in range(1, self.L):
+      for neuron in range(self.n[lay]):
+        for j in range(self.n[lay - 1]):
+          self.h[lay][neuron] += self.w[lay][neuron][j] * self.xi[lay - 1][j]
+
+        self.h[lay][neuron] -= self.theta[lay][neuron]
+        self.xi[lay][neuron] = self.fact(self.h[lay][neuron])
+    
+    return self.xi[self.L - 1]
+
+  # Back−propagation of the error to obtain the delta values
+  def __back_propagation(self, y):
+    # Compute delta in the output layer
+    for neuron in range(self.n[self.L - 1]):
+      self.delta[self.L - 1][neuron] = (self.xi[self.L - 1][neuron] - y[neuron]) * self.derivative(self.h[self.L - 1][neuron])
+
+    # Back-propagate delta to the rest of the network
+    for lay in range(self.L - 2, 0, -1):
+      for neuron in range(self.n[lay]):
+        for j in range(self.n[lay + 1]):
+          self.delta[lay][neuron] += self.w[lay + 1][j][neuron]
         
-        errors_train[epoch][1] /= 2
-          
-        # Feed−forward all validation patterns and calculate their prediction quadratic error
-        if self.validation_set != None:
+        self.delta[lay][neuron] *= self.derivative(self.h[lay][neuron])
 
-          for pat in validation_set:
-            __feed_forward(pat)
+  # Update the weights and thresholds
+  def __update_weights(self):
+    for lay in range(1, self.L):
+      for neuron in range(self.n[lay]):
+        for j in range(self.n[lay - 1]):
+          self.d_w[lay][neuron][j] = -self.learning_rate * self.delta[lay][neuron] * self.xi[lay - 1][j] + self.momentum * self.d_w_prev[lay][neuron][j]
+          self.w[lay][neuron][j] += self.d_w[lay][neuron][j]
+          self.d_w_prev[lay][neuron][j] = self.d_w[lay][neuron][j]
 
-            # Calculate error_valid
-            errors_valid[epoch][0] = epoch
-            errors_valid[epoch][1] = 0
-            
-            for neuron in range(self.n[self.L - 1]):
-              errors_valid[epoch][1] += pow(self.xi[self.L - 1][neuron] - y_valid[pat], 2)
-            
-          errors_valid[epoch][1] /= 2
+        self.d_theta[lay][neuron] = self.learning_rate * self.delta[lay][neuron] + self.momentum * self.d_theta_prev[lay][neuron]
+        self.theta[lay][neuron] -= self.d_theta[lay][neuron]
+        self.d_theta_prev[lay][neuron] = self.d_theta[lay][neuron]
 
-        else:
-          errors_valid[epoch] = None
+  # Predict the output for a given input
+  # X: samples
+  # return: predictions
+  def predict(self, X):
+    predictions = []
 
-    # Feed−forward propagation of pattern xµ to obtain the output o(xµ)
-    def __feed_forward(self, xu):
-      for neuron in range(self.n[0]):
-        self.xi[0][neuron] = xu[neuron]
+    for pat in X:
+      predictions.append(self.__feed_forward(pat))
 
-      for lay in range(1, self.L):
-        for neuron in range(self.n[lay]):
-          for j in range(self.n[lay - 1]):
-            self.h[lay][neuron] += self.w[lay][neuron][j] * self.xi[lay - 1][j]
-
-          self.h[lay][neuron] -= self.theta[lay][neuron]
-          self.xi[lay][neuron] = self.fact(self.h[lay][neuron])
-      
-      return self.xi[self.L - 1]
-
-    # Back−propagation of the error to obtain the delta values
-    def __back_propagation(self, y):
-      # Compute delta in the output layer
-      for neuron in range(self.n[self.L - 1]):
-        self.delta[self.L - 1][neuron] = (self.xi[self.L - 1][neuron] - y[neuron]) * self.derivative(self.h[self.L - 1][neuron])
-
-      # Back-propagate delta to the rest of the network
-      for lay in range(self.L - 2, 0, -1):
-        for neuron in range(self.n[lay]):
-          for j in range(self.n[lay + 1]):
-            self.delta[lay][neuron] += self.w[lay + 1][j][neuron]
-          
-          self.delta[lay][neuron] *= self.derivative(self.h[lay][neuron])
-
-    # Update the weights and thresholds
-    def __update_weights(self):
-      for lay in range(1, self.L):
-        for neuron in range(self.n[lay]):
-          for j in range(self.n[lay - 1]):
-            self.d_w[lay][neuron][j] = -self.learning_rate * self.delta[lay][neuron] * self.xi[lay - 1][j] + self.momentum * self.d_w_prev[lay][neuron][j]
-            self.w[lay][neuron][j] += self.d_w[lay][neuron][j]
-            self.d_w_prev[lay][neuron][j] = self.d_w[lay][neuron][j]
-
-          self.d_theta[lay][neuron] = self.learning_rate * self.delta[lay][neuron] + self.momentum * self.d_theta_prev[lay][neuron]
-          self.theta[lay][neuron] -= self.d_theta[lay][neuron]
-          self.d_theta_prev[lay][neuron] = self.d_theta[lay][neuron]
-
-    # Predict the output for a given input
-    # X: samples
-    # return: predictions
-    def predict(self, X):
-      predictions = []
-
-      for pat in X:
-        predictions.append(__feed_forward(pat))
-
-      return predictions
-      
-    # Return the 
-    def loss_epochs(self):
-      return self.errors_train, self.errors_valid
+    return predictions
+    
+  # Return the errors of the training and validation sets
+  def loss_epochs(self):
+    return self.errors_train, self.errors_valid
       
 
 # layers include input layer + hidden layers + output layer
@@ -204,26 +217,42 @@ derivatives = [
 
 # todo: add read parameters from args
 
-nn = MyNeuralNetwork()  
+nn = MyNeuralNetwork(layers, fact[0], derivatives[0],perc=0.8, epochs=100, learning_rate=0.01, momentum=0.9)  
 
+# read csv with numpy
+data_array = np.loadtxt('A1-turbine-normalized.csv', delimiter=',', skiprows=1)
+X = data_array[:, :-1]
+y = data_array[:, -1]
+
+nn.fit(X, y)
+
+#get errors and plot them
+errors_train, errors_valid = nn.loss_epochs()
+plt.plot(errors_train[:, 0], errors_train[:, 1], label="train")
+plt.plot(errors_valid[:, 0], errors_valid[:, 1], label="validation")
+plt.legend()
+plt.show()
 # todo: execute part
 
 # Check if the neural network is correctly initialized
 print("L = ", nn.L, end="\n")
 print("n = ", nn.n, end="\n")
 
+print("h = ", nn.h, end="\n")
+print("h[1] = ", nn.h[1], end="\n")
+
 print("xi = ", nn.xi, end="\n")
 print("xi[0] = ", nn.xi[0], end="\n")
 print("xi[1] = ", nn.xi[0], end="\n")
 
-print("wh = ", nn.w, end="\n")
-print("wh[1] = ", nn.w[1], end="\n")
+print("w = ", nn.w, end="\n")
+print("w[0] = ", nn.w[0], end="\n")
+print("w[1] = ", nn.w[1], end="\n")
 
 print("theta = ", nn.theta, end="\n")
 print("theta[1] = ", nn.theta[1], end="\n")
 
-print("h = ", nn.h, end="\n")
-print("h[1] = ", nn.h[1], end="\n")
+
 
 print("delta = ", nn.delta, end="\n")
 print("delta[1] = ", nn.delta[1], end="\n")
